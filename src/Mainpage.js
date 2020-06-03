f1e863d5385dd68624dde57d14de225bc7444613
Jp1200 <jprkr96@gmail.com>
2020-06-03 18:57:33 -0500

    bootstrap

---
+++ src/Mainpage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './App.css';
+
+export default class Main extends React.Component {
+constructor(){
+
+  super()
+    this.state({
+      t1l: true
+    })
+
+}
+
+  render(){
+
+    return(
+      <div>
+
+      </div>
+
+    )
+  }
+
+
+
+}
