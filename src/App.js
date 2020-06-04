dd509ea5302f903125b66d039ca1c2ae565b11e7
Jp1200 <jprkr96@gmail.com>
2020-06-04 17:54:16 -0500

    bootstrap and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './Mainpage.js';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Mainpage.js
+++ src/Mainpage.js
@@ -1,23 +1,44 @@
-import React from 'react'
-
-import './App.css';
-
+import React from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 export default class Main extends React.Component {
-constructor(){
 
-  super()
-    this.state({
-      t1l: true
-    })
-
-}
 
   render(){
 
     return(
-      <div>
+      <Container className='p-3'>
+        {/* <Jumbotron>
+          <h1 className='header'>Full-Stack Web Developer, Physicist, Photographer and Musician</h1>
 
-      </div>
+
+        </Jumbotron> */}
+
+        <Row>
+          <Col >
+            <h1 className='header'>Full-Stack Web Developer, Physicist, Photographer and Musician</h1>
+          </Col>
+          <Col align='end'>
+            {/* <Breadcrumb>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>Projects</Breadcrumb.Item>
+              <Breadcrumb.Item>Photography</Breadcrumb.Item>
+              <Breadcrumb.Item>Physics</Breadcrumb.Item>
+            </Breadcrumb> */}
+            <ButtonGroup aria-label="Links" size='lg'>
+              <Button variant="secondary">Home</Button>
+              <Button variant="secondary">Projects</Button>
+              <Button variant="secondary">Photography</Button>
+              <Button variant="secondary">Physics</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+      </Container>
 
     )
   }
