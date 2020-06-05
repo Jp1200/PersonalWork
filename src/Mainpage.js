8f757b6b5ea0cddcceef4c020e64f02b3acd2c64
Jp1200 <jprkr96@gmail.com>
2020-06-04 19:24:30 -0500

    added routing

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -6,6 +6,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+
+// import { Router, Route, Switch } from "react-router";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 export default class Main extends React.Component {
 
 
@@ -18,26 +26,47 @@ export default class Main extends React.Component {
 
 
         </Jumbotron> */}
+        <Router>
+          <Row>
+            <Col >
+              <h1 className='header'>Full-Stack Web Developer, Physicist, Photographer and Musician</h1>
+            </Col>
+
+
+
+            <Col align='end'>
+              {/* <Breadcrumb>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>Projects</Breadcrumb.Item>
+                <Breadcrumb.Item>Photography</Breadcrumb.Item>
+                <Breadcrumb.Item>Physics</Breadcrumb.Item>
+              </Breadcrumb> */}
+              <ButtonGroup aria-label="Links" size='lg'>
+                <Button variant="secondary"><Link to="/">Home</Link></Button>
+                <Button variant="secondary"><Link to="/projects">Projects</Link></Button>
+                <Button variant="secondary"><Link to='/photography'>Photography</Link></Button>
+                <Button variant="secondary"><Link to='/physics'>Physics</Link></Button>
+              </ButtonGroup>
+            </Col>
+
+          </Row>
 
         <Row>
-          <Col >
-            <h1 className='header'>Full-Stack Web Developer, Physicist, Photographer and Musician</h1>
-          </Col>
-          <Col align='end'>
-            {/* <Breadcrumb>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>Projects</Breadcrumb.Item>
-              <Breadcrumb.Item>Photography</Breadcrumb.Item>
-              <Breadcrumb.Item>Physics</Breadcrumb.Item>
-            </Breadcrumb> */}
-            <ButtonGroup aria-label="Links" size='lg'>
-              <Button variant="secondary">Home</Button>
-              <Button variant="secondary">Projects</Button>
-              <Button variant="secondary">Photography</Button>
-              <Button variant="secondary">Physics</Button>
-            </ButtonGroup>
-          </Col>
+          <Switch>
+            {/* Imports for different subjects */}
+            <Route path='projects'>
+
+            </Route>
+            <Route path='/photography'>
+
+            </Route>
+            <Route path='/physics'>
+              
+            </Route>
+
+          </Switch>
         </Row>
+      </Router>
       </Container>
 
     )
