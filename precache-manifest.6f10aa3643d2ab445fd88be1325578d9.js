59e283d66a458873722f870af7075993a74ed6bc
Jp1200 <jprkr96@gmail.com>
2020-06-16 16:32:19 -0500

    Updates

---
+++ precache-manifest.6f10aa3643d2ab445fd88be1325578d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a2af64725b43df9788030f37c442cc",
+    "url": "/PersonalWork/index.html"
+  },
+  {
+    "revision": "2058714612b21a6a912e",
+    "url": "/PersonalWork/static/css/main.8d97dbf1.chunk.css"
+  },
+  {
+    "revision": "be997a8d36f8c51b1322",
+    "url": "/PersonalWork/static/js/2.7ff13f33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PersonalWork/static/js/2.7ff13f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2058714612b21a6a912e",
+    "url": "/PersonalWork/static/js/main.f4b17fb4.chunk.js"
+  },
+  {
+    "revision": "e9424fca4b6fba4f07ce",
+    "url": "/PersonalWork/static/js/runtime-main.812dab8a.js"
+  },
+  {
+    "revision": "abcc762c924374298d960557912f06e7",
+    "url": "/PersonalWork/static/media/IG420.abcc762c.jpg"
+  },
+  {
+    "revision": "fefc0611eb264d888684713eb86c5711",
+    "url": "/PersonalWork/static/media/IG421.fefc0611.jpg"
+  },
+  {
+    "revision": "b4fa4d5833408755aebeeda7df36f3b1",
+    "url": "/PersonalWork/static/media/resume.b4fa4d58.png"
+  }
+]);
