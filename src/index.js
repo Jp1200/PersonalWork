ebdc470257a41188383275ea5a91e9cc24eaf883
Jp1200 <jprkr96@gmail.com>
2020-06-16 16:38:33 -0500

    index.js changes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import {
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.hydrate(
+
+ReactDOM.render(
   <Router>
     <App />
   </Router>,
@@ -18,4 +18,3 @@ ReactDOM.hydrate(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
