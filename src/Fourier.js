1e1db04df88904636a1d8559a15264853b1de8d4
Jp1200 <jprkr96@gmail.com>
2020-06-21 14:33:38 -0500

    latex package and physics buttons work

--- src/Fourier.js
+++ src/Fourier.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import Button from 'react-bootstrap/Button';
-// import sketch from './Resource/FourierSketch.js'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import sketch from './Resource/FourierSketch.js'
 
 export default class Fourier extends React.Component {
   constructor(){
@@ -21,86 +22,92 @@ export default class Fourier extends React.Component {
     })
 
   };
+ //  sketch = (p) => {
+ //    let wave = [];
+ //    let time = 0;
+ //    let deg = this.state.degree
+ //   p.setup = function() {
+ //     p.createCanvas(1200,400);
+ //
+ //     // let input = p.createInput();
+ //     // input.position(200,350);
+ //     // let button = p.createButton('Run');
+ //     // button.position(input.x + input.width, 350);
+ //     // button.mousePressed(changeDegree);
+ //
+ //
+ //
+ //
+ //   };
+ //
+ //   p.draw = function(){
+ //
+ //     p.background(0);
+ //     p.stroke(255);
+ //
+ //     p.translate(200, 200);
+ //     p.line(0,-(100 *  (4 / (1 * p.PI))),0, (100 *  (4 / (1 * p.PI))));
+ //     p.line(0,0,750,0)
+ //
+ //
+ //
+ //     let val = 3
+ //     let x = 0;
+ //     let y = 0;
+ //     for (let i = 0; i < deg; i++) {
+ //       let prevx = x;
+ //       let prevy = y;
+ //       let n = i * 2 + 1;
+ //       let radius = 100 *  (4 / (n * p.PI));
+ //       x += radius * p.cos(n*time);
+ //       y += radius * p.sin(n*time);
+ //
+ //
+ //       p.stroke(255,100);
+ //       p.noFill();
+ //       p.ellipse(prevx, prevy, radius * 2);
+ //
+ //
+ //       p.fill(255);
+ //       p.stroke(255);
+ //       p.line(prevx,prevy,x,y);
+ //       // p.ellipse(x,y,8);
+ //
+ //
+ //     }
+ //     p.beginShape();
+ //     p.noFill();
+ //     wave.unshift(y);
+ //     p.translate(150,0);
+ //     p.line(x-150,y,0, wave[0]);
+ //     for (let i = 0; i< wave.length; i++){
+ //
+ //       p.vertex(i, wave[i]);
+ //     }
+ //
+ //     p.endShape();
+ //     time -= 0.02
+ //
+ //     if (wave.length > 600){
+ //       wave.pop();
+ //     }
+ //   }
+ // }
+  // componentDidMount = () =>{
+  //
+  // }
 
-   sketch = (p) => {
-     let wave = [];
-     let time = 0;
-     let deg = this.state.degree
-    p.setup = function() {
-      p.createCanvas(1200,400);
-
-      // let input = p.createInput();
-      // input.position(200,350);
-      // let button = p.createButton('Run');
-      // button.position(input.x + input.width, 350);
-      // button.mousePressed(changeDegree);
-
-
-
-
-    };
-
-    p.draw = function(){
-
-      p.background(0);
-      p.stroke(255);
-
-      p.translate(200, 200);
-      p.line(0,-(100 *  (4 / (1 * p.PI))),0, (100 *  (4 / (1 * p.PI))));
-      p.line(0,0,750,0)
-
-
-
-      let val = 3
-      let x = 0;
-      let y = 0;
-      for (let i = 0; i < deg; i++) {
-        let prevx = x;
-        let prevy = y;
-        let n = i * 2 + 1;
-        let radius = 100 *  (4 / (n * p.PI));
-        x += radius * p.cos(n*time);
-        y += radius * p.sin(n*time);
-
-
-        p.stroke(255,100);
-        p.noFill();
-        p.ellipse(prevx, prevy, radius * 2);
-
-
-        p.fill(255);
-        p.stroke(255);
-        p.line(prevx,prevy,x,y);
-        // p.ellipse(x,y,8);
-
-
-      }
-      p.beginShape();
-      p.noFill();
-      wave.unshift(y);
-      p.translate(150,0);
-      p.line(x-150,y,0, wave[0]);
-      for (let i = 0; i< wave.length; i++){
-
-        p.vertex(i, wave[i]);
-      }
-
-      p.endShape();
-      time -= 0.02
-
-      if (wave.length > 600){
-        wave.pop();
-      }
-    }
-  }
   render(){
     return(
       <>
-        <Button id='2' onClick={this.changeDegree}>2</Button>
-        <Button id='3' onClick={this.changeDegree}>3</Button>
-        <Button id='4' onClick={this.changeDegree}>4</Button>
-        <Button id='5' onClick={this.changeDegree}>5</Button>
-        <P5Wrapper align='center' sketch={this.sketch}>
+        <ButtonGroup  size='md'>
+          <Button variant="outline-light" id='2' onClick={this.changeDegree}>2</Button>
+          <Button variant="outline-light" id='3' onClick={this.changeDegree}>3</Button>
+          <Button variant="outline-light" id='4' onClick={this.changeDegree}>4</Button>
+          <Button variant="outline-light" id='5' onClick={this.changeDegree}>5</Button>
+          <Button variant="outline-light" id='10' onClick={this.changeDegree}>10</Button>
+        </ButtonGroup>
+        <P5Wrapper className='sketch' sketch={sketch} degree={this.state.degree}>
 
         </P5Wrapper>
       </>
--- src/Physics.js
+++ src/Physics.js
@@ -3,20 +3,30 @@ import Fourier from './Fourier.js';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import Latex from 'react-latex';
 function Physics()  {
   return(
     <Container className='physics'>
       <Row className='physics-heading'>
-
+        <Col>
           <h5>Fourier series</h5>
+        </Col>
+        <Col>
+          <p className='about'>
+            Periodic functions derived from the summation of sinusoidal functions or complex exponential functions. This has many
+            applications in physics and engineering. This is a simple annimation of the convergence of the Fourier series.
+            Clicking on any of the buttons will change the degree of n as represented by this equation. The higher the degree the closer
+            the function will represent a square function i.e. as n => ∞.
 
+          </p>
+        </Col>
       </Row>
-      <Row className='row'>
 
-      </Row>
       <Row >
-        <Fourier/>
+        <Col>
+          <Fourier/>
+        </Col>
+
 
       </Row>
     </Container>
