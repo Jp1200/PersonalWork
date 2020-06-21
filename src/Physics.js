d9ea73989b93d90ac36dc8d44b54024dc86b1c90
Jp1200 <jprkr96@gmail.com>
2020-06-21 18:56:40 -0500

    finished section on fourier

--- src/Physics.js
+++ src/Physics.js
@@ -3,27 +3,42 @@ import Fourier from './Fourier.js';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Latex from 'react-latex';
+
+import MathJax from 'react-mathjax';
 function Physics()  {
+  const fourierA = 'a_n = \\frac{2}{P}\\int_P s(x)\\cdot \\cos(2\\pi x \\frac{n}{P}) dx';
+  const fourierB = 'b_n = \\frac{2}{P}\\int_P s(x)\\cdot \\sin(2\\pi x \\frac{n}{P}) dx';
+  const c = '\\frac{\\sin(n \\theta)}{n \\pi}';
+  const series = 's_N(x) = \\frac{a_O}{2} + \\sum_{n=1}^{N}(a_n\\cos(2\\pi x \\frac{n}{P}) + b_n\\sin(2\\pi x \\frac{n}{P}))';
   return(
     <Container className='physics'>
       <Row className='physics-heading'>
         <Col>
           <h5>Fourier series</h5>
-          <h5>
-            <Latex >
-              $(3\times 4) \div (5-3)$
-            </Latex>
+          <h5 className='math'>
+            <MathJax.Provider >
+              <MathJax.Node formula={fourierA}/>
+              <MathJax.Node formula={fourierB}/>
+            </MathJax.Provider>
           </h5>
+          <p className='physics-text'>
+            Here listed are the fourier coeffcients used to solve differiential equations. Used primarily for approximating equations.
+            Usage includes solving the heat equation. P represents the periodicity (often set equal to 2π for a full period).
+            s(x) is a real valued function that is integrable on length P.
+
+          </p>
         </Col>
         <Col>
-          <p className='about'>
+
+          <MathJax.Provider><p className='physics-text'>
             Periodic functions derived from the summation of sinusoidal functions or complex exponential functions. This has many
             applications in physics and engineering. This is a simple annimation of the convergence of the Fourier series.
             Clicking on any of the buttons will change the degree of n as represented by this equation. The higher the degree the closer
-            the function will represent a square function i.e. as n => ∞.
+            the function will represent a square function i.e. as N => ∞. <MathJax.Node formula={c}/> Finding the coeffcients is trivial for most s(x).
+            <MathJax.Node formula={series}/>
 
-          </p>
+          </p></MathJax.Provider>
+
         </Col>
       </Row>
 
@@ -32,6 +47,9 @@ function Physics()  {
           <Fourier/>
         </Col>
 
+
+      </Row>
+      <Row>
 
       </Row>
     </Container>
