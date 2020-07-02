bb57ef783c65cffa98dc8cd463735cc548b3adba
Jp1200 <jprkr96@gmail.com>
2020-07-02 17:08:09 -0500

    corrected shifting

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -1,56 +1,40 @@
-import React from 'react';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-
-import Media from 'react-bootstrap/Media';
-import Image from 'react-bootstrap/Image';
-import Modal from 'react-bootstrap/Modal';
-import { SocialIcon } from 'react-social-icons';
-
+import Media from "react-bootstrap/Media";
+import Image from "react-bootstrap/Image";
+import Modal from "react-bootstrap/Modal";
+import { SocialIcon } from "react-social-icons";
 
 // File imports ######
-import Projects from './Projects.js';
-import Photography from './Photography.js';
-import Physics from './Physics.js';
+import Projects from "./Projects.js";
+import Photography from "./Photography.js";
+import Physics from "./Physics.js";
 // import { Router, Route, Switch } from "react-router";
-import {
-
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 export default class Main extends React.Component {
-
-
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       show: false,
-      width: window.innerWidth
-
-    }
+      width: window.innerWidth,
+    };
   }
-
 
   handleClose = () => {
-    this.setState({show:false});
-  }
+    this.setState({ show: false });
+  };
   // const handleShow = () => setShow(true);
-  handleShow = () =>{
-     this.setState({show:true});
-
-  }
-  render(){
-
-
-
-
-    return(
-      <Container fluid className='mainpage'>
+  handleShow = () => {
+    this.setState({ show: true });
+  };
+  render() {
+    return (
+      <Container fluid className="mainpage">
         {/* <Jumbotron>
           <h1 className='header'>Full-Stack Web Developer, Physicist, Photographer and Musician</h1>
 
@@ -58,111 +42,125 @@ export default class Main extends React.Component {
         </Jumbotron> */}
         {/* {particles} */}
 
+        <Row className="row">
+          <Col className="col-xs-6">
+            <h3 className="header text-center">
+              Jack Parker
+              <SocialIcon
+                className="icons"
+                url="https://www.instagram.com/jack_p1996/"
+              />
+              <SocialIcon className="icons" url="https://github.com/Jp1200" />
+              <SocialIcon
+                className="icons"
+                url="https://www.linkedin.com/in/jack-parker-473523183/"
+              />
+              <SocialIcon
+                className="icons"
+                url="https://neighborhood-farmer-market.herokuapp.com/"
+              />
+            </h3>
+          </Col>
 
-          <Row className='row'>
-            <Col className="col-xs-6">
-              <h3 className='header text-center'>Jack Parker
-                <SocialIcon className='icons' url="https://www.instagram.com/jack_p1996/"/>
-                <SocialIcon className='icons' url="https://github.com/Jp1200"/>
-                <SocialIcon className='icons' url="https://www.linkedin.com/in/jack-parker-473523183/"/>
-                <SocialIcon className='icons' url="https://neighborhood-farmer-market.herokuapp.com/"/>
-              </h3>
-
-            </Col>
-
-
-
-            <Col className='col-xs-6 '>
-              {/* <Breadcrumb>
+          <Col className="col-xs-6 ">
+            {/* <Breadcrumb>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>Projects</Breadcrumb.Item>
                 <Breadcrumb.Item>Photography</Breadcrumb.Item>
                 <Breadcrumb.Item>Physics</Breadcrumb.Item>
               </Breadcrumb> */}
-              <ButtonGroup aria-label="Links" size='lg'>
-                <Button className="button-header" variant="outline-light"><Link className="link" to="/">Home</Link></Button>
-                <Button className="button-header"  variant="outline-light"><Link className="link" to="/projects">Projects</Link></Button>
-                <Button className="button-header"  variant="outline-light"><Link className="link" to='/photography'>Photography</Link></Button>
-                <Button className="button-header"  variant="outline-light"><Link className="link" to='/physics'>Physics</Link></Button>
-              </ButtonGroup>
-            </Col>
-
-          </Row>
-          <h3 align='center' className='page-wrapper'>Full-Stack Web Developer, Physicist, Photographer and Musician</h3>
-          <Modal
-            size='lg'
-            aria-labelledby='contained-modal-title-vcenter'
-            show={this.state.show} onHide={this.handleClose} >
-
-            <Image  width={805} height={1025} src={require('./resume.png')}/>
-
-          </Modal>
+            <ButtonGroup aria-label="Links" size="lg">
+              <Button className="button-header" variant="outline-light">
+                <Link className="link" to="/">
+                  Home
+                </Link>
+              </Button>
+              <Button className="button-header" variant="outline-light">
+                <Link className="link" to="/projects">
+                  Projects
+                </Link>
+              </Button>
+              <Button className="button-header" variant="outline-light">
+                <Link className="link" to="/photography">
+                  Photography
+                </Link>
+              </Button>
+              <Button className="button-header" variant="outline-light">
+                <Link className="link" to="/physics">
+                  Physics
+                </Link>
+              </Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <h3 align="center" className="page-wrapper">
+          Full-Stack Web Developer, Physicist, Photographer and Musician
+        </h3>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          show={this.state.show}
+          onHide={this.handleClose}
+        >
+          <Image width={805} height={1025} src={require("./resume.png")} />
+        </Modal>
 
         <Row>
           <Switch>
             {/* Imports for different subjects */}
-            <Route exact path='/'>
-
-
-
+            <Route exact path="/">
               <Container>
-                <br/>
-                <h2 align='center' className='resume-modal'>Resumé</h2>
-                <Row className='row'>
+                <br />
+                <h2 align="center" className="resume-modal">
+                  Resumé
+                </h2>
+                <Row className="row">
+                  <br />
+                  <br />
+                  <br />
 
-
-                  <br/>
-                  <br/>
-                  <br/>
-
-                  <br/>
-                  <Media
-                    className='media'>
+                  <br />
+                  <Media className="media">
                     <Image
-
                       onClick={this.handleShow}
-                      src={require('./resume.png')}
-
-                      width={805/4}
-                      height={1025/4}
-                      />
+                      src={require("./resume.png")}
+                      width={805 / 4}
+                      height={1025 / 4}
+                    />
                   </Media>
                 </Row>
                 <Row>
                   <Col>
-                  <h1 className='about-h1'>About me ---></h1>
-                  <p className='about'>
-                    Trinity University graduate student with a background in physics and mathematics. I currently live in Austin, Texas and am in the process of a job search.
-                    I have many hobbies which include photography, playing piano/guitar and playing video games. I love learning and pursing knowledge I find interesting.
-                    This website was designed
-                    to look empty and barren as I wish to add more and more to it over time. The sky might be the limit, however, space is limitless.
-                    Using ReactJS and BootStrap-ReactJS and implementing SCSS to create the background. Mobile view is atrocious, currently
-                    working on improving it.
-                  </p>
+                    <h1 className="about-h1">About me ---></h1>
+                    <p className="about">
+                      Trinity University graduate student with a background in
+                      physics and mathematics. I currently live in Austin, Texas
+                      and am in the process of a job search. I have many hobbies
+                      which include photography, playing piano/guitar and
+                      playing video games. I love learning and pursing knowledge
+                      I find interesting. This website was designed to look
+                      empty and barren as I wish to add more and more to it over
+                      time. The sky might be the limit, however, space is
+                      limitless. Using ReactJS and BootStrap-ReactJS and
+                      implementing SCSS to create the background. Mobile view is
+                      atrocious, currently working on improving it.
+                    </p>
                   </Col>
                 </Row>
               </Container>
             </Route>
-            <Route exact path='/projects'>
-              <Projects/>
+            <Route exact path="/projects">
+              <Projects />
             </Route>
-            <Route exact path='/photography'>
-
-              <Photography/>
+            <Route exact path="/photography">
+              <Photography />
             </Route>
-            <Route exact path='/physics'>
-              <Physics/>
+            <Route exact path="/physics">
+              <Physics />
             </Route>
-
           </Switch>
         </Row>
-
-
       </Container>
-
-    )
+    );
   }
-
-
-
 }
--- src/Physics.js
+++ src/Physics.js
@@ -1,59 +1,61 @@
-import React from 'react';
-import Fourier from './Fourier.js';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import Fourier from "./Fourier.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import MathJax from 'react-mathjax';
-function Physics()  {
-  const fourierA = 'a_n = \\frac{2}{P}\\int_P s(x)\\cdot \\cos(2\\pi x \\frac{n}{P}) dx';
-  const fourierB = 'b_n = \\frac{2}{P}\\int_P s(x)\\cdot \\sin(2\\pi x \\frac{n}{P}) dx';
-  const c = '\\frac{\\sin(n \\theta)}{n \\pi}';
-  const series = 's_N(x) = \\frac{a_O}{2} + \\sum_{n=1}^{N}(a_n\\cos(2\\pi x \\frac{n}{P}) + b_n\\sin(2\\pi x \\frac{n}{P}))';
-  return(
-    <Container className='physics'>
-      <Row className='physics-heading'>
+import MathJax from "react-mathjax";
+function Physics() {
+  const fourierA =
+    "a_n = \\frac{2}{P}\\int_P s(x)\\cdot \\cos(2\\pi x \\frac{n}{P}) dx";
+  const fourierB =
+    "b_n = \\frac{2}{P}\\int_P s(x)\\cdot \\sin(2\\pi x \\frac{n}{P}) dx";
+  const c = "\\frac{\\sin(n \\theta)}{n \\pi}";
+  const series =
+    "s_N(x) = \\frac{a_O}{2} + \\sum_{n=1}^{N}(a_n\\cos(2\\pi x \\frac{n}{P}) + b_n\\sin(2\\pi x \\frac{n}{P}))";
+  return (
+    <Container>
+      <Row align="center" className="physics-heading">
         <Col>
           <h5>Fourier series</h5>
-          <h5 className='math'>
-            <MathJax.Provider >
-              <MathJax.Node formula={fourierA}/>
-              <MathJax.Node formula={fourierB}/>
+          <h5 className="math">
+            <MathJax.Provider>
+              <MathJax.Node formula={fourierA} />
+              <MathJax.Node formula={fourierB} />
             </MathJax.Provider>
           </h5>
-          <p className='physics-text'>
-            Here listed are the fourier coeffcients used to solve differiential equations. Used primarily for approximating equations.
-            Usage includes solving the heat equation. P represents the periodicity (often set equal to 2π for a full period).
-            s(x) is a real valued function that is integrable on length P.
-
+          <p className="physics-text">
+            Here listed are the fourier coeffcients used to solve differiential
+            equations. Used primarily for approximating equations. Usage
+            includes solving the heat equation. P represents the periodicity
+            (often set equal to 2π for a full period). s(x) is a real valued
+            function that is integrable on length P.
           </p>
         </Col>
         <Col>
-
-          <MathJax.Provider><p className='physics-text'>
-            Periodic functions derived from the summation of sinusoidal functions or complex exponential functions. This has many
-            applications in physics and engineering. This is a simple annimation of the convergence of the Fourier series.
-            Clicking on any of the buttons will change the degree of n as represented by this equation. The higher the degree the closer
-            the function will represent a square function i.e. as N => ∞. <MathJax.Node formula={c}/> Finding the coeffcients is trivial for most s(x).
-            <MathJax.Node formula={series}/>
-
-          </p></MathJax.Provider>
-
+          <MathJax.Provider>
+            <p className="physics-text">
+              Periodic functions derived from the summation of sinusoidal
+              functions or complex exponential functions. This has many
+              applications in physics and engineering. This is a simple
+              annimation of the convergence of the Fourier series. Clicking on
+              any of the buttons will change the degree of n as represented by
+              this equation. The higher the degree the closer the function will
+              represent a square function i.e. as N => ∞.{" "}
+              <MathJax.Node formula={c} /> Finding the coeffcients is trivial
+              for most s(x).
+              <MathJax.Node formula={series} />
+            </p>
+          </MathJax.Provider>
         </Col>
       </Row>
 
-      <Row >
+      <Row align="center">
         <Col>
-          <Fourier/>
+          <Fourier />
         </Col>
-
-
-      </Row>
-      <Row>
-
       </Row>
     </Container>
-  )
-
+  );
 }
 export default Physics;
