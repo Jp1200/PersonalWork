af3e7ff60d8c194acc4a3fe139abea2970d30077
Jp1200 <jprkr96@gmail.com>
2020-08-05 12:41:15 -0500

    added vrofi project to project tab

--- src/Projects.js
+++ src/Projects.js
@@ -34,7 +34,8 @@ export default class Projects extends React.Component {
             </a>
 
             <p className="about">
-              HomeGrown Foods, App for neighbors to trade produce
+              HomeGrown Foods, App for neighbors to trade produce. Heroku Server
+              host is slow(refreshing and clicking again will fix it)
             </p>
 
             <a href="https://github.com/Jp1200/kareoji">
@@ -47,6 +48,20 @@ export default class Projects extends React.Component {
             </a>
 
             <p className="about">Kareoji, karaoke but with emojis</p>
+
+            <a href="https://github.com/Jp1200/Jp1200-ShowsInZip">
+              <Image
+                align="center"
+                className="imageX"
+                src={require("./Resource/VroFi.png")}
+                thumbnail
+              ></Image>
+            </a>
+
+            <p className="about">
+              VroFi, Venue/show finder for your zip code allows to preview band
+              music before going to show
+            </p>
           </Col>
         </Row>
         <Row align="center"></Row>
